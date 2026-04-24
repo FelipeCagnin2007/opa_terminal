d4877e83a996f40d8356a6878adaee7c15247b0b
FelipeCagnin2007 <cagnin.lima.2007@gmail.com>
2026-04-24 19:55:33 -0300

    Delete ALL

--- opa-terminal-react/src/utils/petConstants.js
+++
@@ -1,8 +0,0 @@
-export const PET_ASCII = {
-    EGG: "   .---.   \n  /     \\  \n |   O   | \n  \\     /  \n   '---'   ",
-    LEXER_BABY: "    \\^.^/   \n    ( o )   \n     vvv    ",
-    PROTOCOL_GUARDIAN: "     /\\_/\\     \n    ( o.o )    \n     > ^ <     \n    /     \\    \n   |       |   \n   '-------'   ",
-    SYNTAX_KNIGHT: "      /\\      \n     |  |     \n    /----\\    \n   [ o  o ]   \n    \\_--_/    \n     |  |     \n    /|  |\\    ",
-    NODE_WIZARD: "     .---.     \n    /     \\    \n   | (O_O) |   \n    \\  -  /    \n    /|:::|\\    \n   / |:::| \\   \n     '---'     ",
-    QUANTUM_MASTER: "   .-------.   \n  /   _-_   \\  \n |  ( o o )  | \n  \\   `-'   /  \n   '-------'   \n    /  |  \\    \n   o   o   o   "
-};
